100ae04dfa91063a1bf24aa55d98ac13121cba21
Raka Purusadhikara Putra <rakaprsdhkra@gmail.com>
2020-07-17 16:08:35 +0700

    innitial commit

---
+++ nodejs-exercises2.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+app.get('/', function(req, res){
+    res.send('Ini adalah Halaman Home!');    
+});
+
+app.get('/product', function(req, res){
+    res.send('Ini adalah Halaman Produk!');    
+});
+
+app.get('/detailproduct', function(req, res){
+    res.send('Ini adalah Halaman Detail Produk Toko Anda!');    
+});
+
+app.listen(8000, function(){
+    console.log('Aplikasi ini berjalan pada port 8000!');
+}); 
